baf0e4b0414c3b578046e18d93b0802a5dd8396e
ra1ny-yuki <wyb99@hotmail.com>
2023-12-06 23:34:03 +0800

    support MCDReforged v2.15

--- my_plugin/utils/translation.py
+++ my_plugin/utils/translation.py
@@ -1,5 +1,6 @@
 import re
 from typing import Union, Optional, List, Dict
+
 from mcdreforged.api.rtext import *
 
 from my_plugin.utils.misc import psi
@@ -42,12 +43,15 @@ def rtr(translation_key: str, *args, with_prefix: bool = True, **kwargs) -> RTex
 
 
 def ntr(
-        translation_key: str, *args, language: Optional[str] = None, allow_failure: bool = True,
-        default_fallback: Optional[MessageText] = None, log_error_message: bool = True, **kwargs
+        translation_key: str, *args, language: Optional[str] = None, _mcdr_tr_language: Optional[str] = None,
+        allow_failure: bool = True, _default_fallback: Optional[MessageText] = None, log_error_message: bool = True, **kwargs
 ) -> MessageText:
+    if language is not None and _mcdr_tr_language is None:
+        _mcdr_tr_language = language
     try:
         return psi.tr(
-            translation_key, *args, language=language, allow_failure=False, **kwargs
+            translation_key, *args, language=_mcdr_tr_language,
+            _mcdr_tr_language=_mcdr_tr_language, allow_failure=False, **kwargs
         )
     except (KeyError, ValueError):
         fallback_language = psi.get_mcdr_language()
@@ -55,20 +59,21 @@ def ntr(
             if fallback_language == 'en_us':
                 raise KeyError(translation_key)
             return psi.tr(
-                translation_key, *args, language='en_us', allow_failure=False, **kwargs
+                translation_key, *args, _mcdr_tr_language='en_us',
+                language='en_us', allow_failure=False, **kwargs
             )
         except (KeyError, ValueError):
             languages = []
-            for item in (language, fallback_language, 'en_us'):
+            for item in (_mcdr_tr_language, fallback_language, 'en_us'):
                 if item not in languages:
                     languages.append(item)
             languages = ', '.join(languages)
             if allow_failure:
                 if log_error_message:
                     psi.logger.error(f'Error translate text "{translation_key}" to language {languages}')
-                if default_fallback is None:
+                if _default_fallback is None:
                     return translation_key
-                return default_fallback
+                return _default_fallback
             else:
                 raise KeyError(f'Translation key "{translation_key}" not found with language {languages}')
 
@@ -91,9 +96,12 @@ def dtr(translation_dict: Dict[str, str], *args, **kwargs) -> RTextMCDRTranslati
             translation_key: str,
             *inner_args,
             language: Optional[str] = None,
+            _mcdr_tr_language: Optional[str] = None,
             allow_failure: bool = True,
             **inner_kwargs
     ) -> MessageText:
+        if language is not None and _mcdr_tr_language is None:
+            _mcdr_tr_language = language
         result = translation_dict.get(language)
         fallback_language = [psi.get_mcdr_language()]
         if 'en_us' not in fallback_language and 'en_us' != language:
